a874158b70d9236dbc053800e94d2ef20a21166d
Polyhalov <apolyhalov@gmail.com>
2023-08-11 13:07:05 +0300

    skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,21 @@ import '../scss/app.scss';
 import Categories from './Categories/Categories';
 import Header from './Header/Header';
 import PizzaBlock from './PizzaBlock/PizzaBlock';
+import Skeleton from './PizzaBlock/Skeleton';
 import Sort from './Sort/Sort';
 import { useEffect, useState } from 'react';
 
 export const App = () => {
 
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://64d5f14a754d3e0f13615c96.mockapi.io/items').then((res) => res.json()).then((arr) => { setItems(arr) });
+    fetch('https://64d5f14a754d3e0f13615c96.mockapi.io/items').then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
   },[])
   return (
     <div className="wrapper">
@@ -23,7 +29,8 @@ export const App = () => {
           </div>
           <h2 className="content__title">Всі піцци</h2>
           <div className="content__items">
-            {items.map((item) => (<PizzaBlock key={item.id} {...item} />))}
+            {isLoading ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : items.map((item) => <PizzaBlock key={item.id} {...item} />)}
           </div>
         </div>
       </div>
